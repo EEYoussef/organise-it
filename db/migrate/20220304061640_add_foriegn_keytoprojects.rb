2faf057c32a7e0f69b30be23f970ab701086129e
Eman Youssef <emanessam@hotmail.com>
2022-03-09 18:29:18 +1000

    migration changes

--- db/migrate/20220304061640_add_foriegn_keytoprojects.rb
+++ db/migrate/20220304061640_add_foriegn_keytoprojects.rb
@@ -1,5 +1,7 @@
 class AddForiegnKeytoprojects < ActiveRecord::Migration[6.1]
+  
   def change
+    add_reference :projects, :freelanceruser_id, index: true
     add_foreign_key :projects, :users , column: :freelanceruser_id
   end
 
